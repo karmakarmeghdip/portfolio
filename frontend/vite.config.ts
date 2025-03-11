a0f92d4d9b255c531dc96b9e123a367e9e4b1a50
karmakarmeghdip <karmakarmeghdip@gmail.com>
2025-03-12 02:42:10 +0530

    feat: migrate frontend to Deno and Vite, update configurations and dependencies

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // @ts-ignore: Bullshit type error
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
