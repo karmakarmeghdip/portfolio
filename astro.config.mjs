0347fe48567b10a9515e7f0741069ce53ae03c98
karmakarmeghdip <karmakarmeghdip@gmail.com>
2025-03-13 16:21:59 +0530

    feat: add Deno adapter and update deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
+import deno from '@deno/astro-adapter';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,7 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  output: 'server',
+  adapter: deno()
 });
