1d1ba71ad2ac3d64e73ce980d48d9cd4e80d10fb
karmakarmeghdip <karmakarmeghdip@gmail.com>
2025-03-18 21:40:25 +0530

    feat: add error handling to GitHub activity card and improve theme preference logging in layout

--- src/components/github.tsx
+++ src/components/github.tsx
@@ -3,20 +3,26 @@ import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter }
 import GithubCalendar from "react-github-calendar"
 
 export const GithubCard = () => {
-  return <Card>
-    <CardHeader>
-      <CardTitle>GitHub</CardTitle>
-      <CardDescription>My recent activity</CardDescription>
-    </CardHeader>
-    <CardContent className="h-40 flex items-center justify-center bg-muted/20">
-      <GithubCalendar username="karmakarmeghdip" />
-    </CardContent>
-    <CardFooter>
-      <Button variant="outline" size="sm" asChild>
-        <a href="https://github.com/karmakarmeghdip" target="_blank" rel="noopener noreferrer">
-          View Profile
-        </a>
-      </Button>
-    </CardFooter>
-  </Card>
+  try {
+    return <Card>
+      <CardHeader>
+        <CardTitle>GitHub</CardTitle>
+        <CardDescription>My recent activity</CardDescription>
+      </CardHeader>
+      <CardContent className="h-40 pt-3 flex items-center justify-center bg-muted/20">
+
+        <GithubCalendar username="karmakarmeghdip" />
+      </CardContent>
+      <CardFooter>
+        <Button variant="outline" size="sm" asChild>
+          <a href="https://github.com/karmakarmeghdip" target="_blank" rel="noopener noreferrer">
+            View Profile
+          </a>
+        </Button>
+      </CardFooter>
+    </Card>
+  } catch (error) {
+    console.log("Error", error)
+    return <></>
+  }
 }
